f5ad40ff47d73a3051e2634a0b7d8a597a51a982
Michael Sila <16419696+JustAnotherSoftwareDeveloper@users.noreply.github.com>
2018-03-22 17:16:03 -0600

    Set up landing page along with new icon

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Header} from "semantic-ui-react";
+import { Input, Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header block as="h1">Recipe Database</Header>
+      <div className="App landing-page">
+        <Container className="search-bar">
+          <Input focus 
+          placeholder="Search by Tag or Ingredient..."
+          iconPosition="left"
+          icon={{name: 'food',color:"red",fluid:true}}
+          fluid
+          size="massive"
+          />
+        </Container>
       </div>
     );
   }
